fe5c898bfd7fee87ca7d0e8867d19eb87ac91fae
Elizabeth <itsalaz@icloud.com>
2024-05-08 15:59:14 -0400

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+// destructivelyAppendCat('Max')
+//   console.log(cats)
+
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.push(name)
+  return copyOfCats
+}
+
+function prependCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.unshift(name)
+  return copyOfCats
+}
+
+function removeLastCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.pop(name)
+  return copyOfCats
+}
+
+function removeFirstCat() {
+  const copyOfCats = [...cats]
+  copyOfCats.shift()
+  return copyOfCats
+}
